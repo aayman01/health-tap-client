72f11d4b2fec1e8b540dc22df83ef252c65d8702
aayman01 <ayman.alfi098@gmail.com>
2024-05-09 19:52:57 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl">Vite + React</h1>
+      <button className="btn btn-secondary">Two</button>
+    </>
+  );
+}
+
+export default App
