26c9e07fea9fe77202cb9fbf6fd48cc8839a87d6
aayman01 <ayman.alfi098@gmail.com>
2024-05-09 23:52:22 +0600

    footer complete

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import Footer from "../Shared/Footer/Footer";
 import NavBar from "./NavBar";
 
 const Home = () => {
     return (
         <div>
             <NavBar/>
+            <Footer/>
         </div>
     );
 };
